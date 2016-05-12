40f8b4911142cbc5f53445eb6e9bc4a5550de525
Clemens Lange <clemens.lange@cern.ch>
2016-05-12 18:54:16 +0200

    add simple conversion script to speed up things a little

---
+++ CtoPy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+FILE=$1
+echo $FILE
+
+sed -i '.bak' 's|//|#|g;s|->|.|g' ${FILE}
+# this screws up formulae
+# sed -i '.bak' 's|*||g;' ${FILE}
+sed -i '.bak' 's|(Title|(RooFit.Title|g' ${FILE}
+sed -i '.bak' 's|,Save()|,RooFit.Save()|g' ${FILE}
+sed -i '.bak' 's|,.RooConst(|,RooFit.RooConst(|g' ${FILE}
+sed -i '.bak' 's/;*$//g' ${FILE}
+sed -i '.bak' 's/generate(x/generate(RooArgSet(x)/g' ${FILE}
+
+rm ${FILE}.bak
