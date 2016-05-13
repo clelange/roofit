f6b264b052993a227b3d2e896129f82eb60e487b
Clemens Lange <clemens.lange@cern.ch>
2016-05-13 18:50:54 +0200

    avoid import *

--- CtoPy.sh
+++ CtoPy.sh
@@ -21,5 +21,11 @@ sed -i '.bak' 's/;*$//g' ${FILE}
 sed -i '.bak' 's/generate(x/generate(RooArgSet(x)/g' ${FILE}
 sed -i '.bak' 's/createIntegral(x/createIntegral(RooArgSet(x)/g' ${FILE}
 sed -i '.bak' 's/using namespace RooFit/from ROOT import */g' ${FILE}
+sed -i '.bak' 's/ T/ ROOT.T/g' ${FILE}
+sed -i '.bak' 's/ gPad/ ROOT.gPad/g' ${FILE}
+sed -i '.bak' 's/ Roo/ ROOT.Roo/g' ${FILE}
+sed -i '.bak' 's/(Roo/(ROOT.Roo/g' ${FILE}
+sed -i '.bak' 's/,Roo/,ROOT.Roo/g' ${FILE}
+sed -i '.bak' 's/Color(k/Color(ROOT.k/g' ${FILE}
 
 rm ${FILE}.bak
